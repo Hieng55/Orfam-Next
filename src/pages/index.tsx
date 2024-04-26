62a5315d9937826e576cafe61596884f6d67bea0
Hieng55 <bosshieng55@gmail.com>
2024-04-26 21:06:46 +0700

    opengraph

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,8 @@ import AboutUs from "@/components/features/home/about";
 import SpecialProducts from "@/components/features/home/sliderProducts";
 import BlogPost from "@/components/features/home/blogPost";
 
+import homeGraph from "@/image/graph/homeGraph.png";
+
 const Home = () => {
   return (
     <>
@@ -22,10 +24,10 @@ const Home = () => {
           property="og:description"
           content="Home page specializing in selling vegetables - Shop for fresh, quality vegetables from the farm to your table. Explore now!"
         />
-        <meta
-          name="facebook:image:type"
-          content="Home page specializing in selling vegetables - Shop for fresh, quality vegetables from the farm to your table. Explore now!"
-        />
+        <meta name="facebook:image:type" content={`${homeGraph}`} />
+        <meta name="instagram:image:type" content={`${homeGraph}`} />
+        <meta name="linked:image:type" content={`${homeGraph}`} />
+        <meta name="og:image" content={`${homeGraph}`} />
       </Head>
       <ToastContainer />
       <Banner />
