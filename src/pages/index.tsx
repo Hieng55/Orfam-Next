657e9c0b4a9c466d66eb612b8d8df59d7144e64b
Hieng55 <bosshieng55@gmail.com>
2024-04-30 11:33:37 +0700

    fix-meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,8 @@ import AboutUs from "@/components/features/home/about";
 import SpecialProducts from "@/components/features/home/sliderProducts";
 import BlogPost from "@/components/features/home/blogPost";
 
+import graph from "@/image/graph/homeGraph.png";
+
 const Home = () => {
   return (
     <>
@@ -22,9 +24,8 @@ const Home = () => {
           property="og:description"
           content="Home page specializing in selling vegetables - Shop for fresh, quality vegetables from the farm to your table. Explore now!"
         />
-        <meta name="facebook:image:type" content="image/png" />
-        <meta name="instagram:image:type" content="image/png" />
-        <meta name="linked:image:type" content="image/png" />
+        <meta property="og:url" content={`${graph}`} />
+        <meta property="og:image" content={`${graph}`} />
       </Head>
       <ToastContainer />
       <Banner />
